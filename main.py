585dd1b8f2951fad574906a11bce528c16dbcf65
S-Kiev <vezequiel58@gmail.com>
2024-11-08 20:55:36 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+# URL del sitio Moodle para descargar grabaciones
+url = "https://api.frankfurter.app/latest?symbols=CHF"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Grabaciones descargadas correctamente.")
+else:
+    print(f"Error al descargar las grabaciones. Código de estado: {response.status_code}")
