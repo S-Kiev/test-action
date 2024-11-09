2a14a9f147c64caf21925576d56a4af2d80fca4e
S-Kiev <vezequiel58@gmail.com>
2024-11-08 23:24:31 -0300

    hola

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import requests
 # URL del sitio Moodle para descargar grabaciones
 url = "https://api.frankfurter.app/latest?symbols=CHF"
 
+
+
 response = requests.get(url)
 
 if response.status_code == 200:
     print("Grabaciones descargadas correctamente.")
+    print (response.json())
 else:
     print(f"Error al descargar las grabaciones. Código de estado: {response.status_code}")
